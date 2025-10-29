4b19caaccd060e6c6a922c63e2b42fb4429a14ee
Nathaneil2025 <seanbad85@gmail.com>
2025-10-29 18:16:46 +0200

    helm fixed4

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({
-        'message': 'Welcome to Flask App on EKS! new ver1',
+        'message': 'Welcome to Flask App on EKS! new ver2',
         'status': 'running',
         'hostname': socket.gethostname(),
         'version': os.getenv('APP_VERSION', '1.0.0')
